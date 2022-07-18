90b5c947e48f74bb4c07041c501256a376c7a667
cexperto <sipuedeshazloya@gmail.com>
2022-07-17 20:55:19 -0500

    login, login required for endpoints

--- products/views.py
+++ products/views.py
@@ -6,16 +6,20 @@ import sqlite3
 
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
+# Create your views here.
+@login_required
 def index(request):
     data = list(Products.objects.values())
     return JsonResponse(data, safe=False)
 
+@login_required
 def detaills(request, id):
     product = get_object_or_404(Products, pk=id)
     return render(request, 'products/detaill.html', {'product': product})
 
+@login_required
 def order(request, field):
     fields = ['index', 'titles', 'categories', 'price', 'description', 'images', 'reviews', 'ratings']
     if field not in fields:
@@ -23,7 +27,7 @@ def order(request, field):
     products = Products.objects.all().order_by(field)[:5].values()    
     return JsonResponse({'data': list(products)}, safe=False)
 
-
+@login_required
 def load_csv(request):
     jf = json_formated()
     insert_df(jf)
