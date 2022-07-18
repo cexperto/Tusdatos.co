6285563fb9d9e1e139a8d5ed2450341c162c9153
cexperto <sipuedeshazloya@gmail.com>
2022-07-17 22:27:19 -0500

    test, some test added for products

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Products
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class ProductsModel(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create_user('usertest', 'usertest@gmail.com', '1234')
+
+    def test_products(self):
+        products = Products(
+            index = 1,
+            categories = 'phones',
+            titles = 'nokia',
+            price = '22.50',
+            description = 'is a new phone',
+            images = 'www.somepage.com/img.jpg',
+            reviews = '7 reviews',
+            ratings = '2'
+        )
+        self.assertIs(products.categories, 'phones') 
+    
+    def test_products_views(self):        
+        self.client.login(username='usertest', password='1234')
+        response = self.client.get('/products/', follow=True)
+        self.assertEqual(response.status_code, 200)
